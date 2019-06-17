924ff9694b56acdb52a95ca1307bf3e5bb28dd4c
duybn-0885 <nd26041998@gmail.com>
2019-06-17 09:04:09 +0700

    add rspec view v1

--- spec/factories/restaurant.rb
+++ spec/factories/restaurant.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :restaurant do
     name {FFaker::Company.name}
     location {FFaker::Address.street_address}
-    open_time {12000}
-    close_time {24000}
+    open_time {36000}
+    close_time {72000}
   end
 end
